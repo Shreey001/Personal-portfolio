89f7184208f5edb525dc6a26768ed7f123ed087b
ujjwal bhandari <80826873+ujjwalbhandarii@users.noreply.github.com>
2023-03-17 17:20:43 +0545

    test

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
+function SendMail() {
+  let params = {
+    from_name: document.getElementById("Name").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("Message").value,
+  };
+  const serviceID = "service_j2jrsdl";
+  const templateID = "template_5umnd5e";
 
-    function SendMail(){
-        let params = {
-            name : document.getElementById("Name").value,
-            email_id : document.getElementById("email").value,
-            message : document.getElementById("Message").value 
-        };
-       const serviceID = "service_j2jrsdl";
-       const templateID="template_5umnd5e";
-
-       emailjs.send(serviceID,templateID,params)
-       .then( (res) => {
-        if(res.status == 200){
-            document.getElementById("Name").value="";
-            document.getElementById("email").value="";
-            document.getElementById("Message").value="";
-            console.log(res);
-            alert("your message sent successfully");
-        }
-        else{
-            console.log("gayana text")
-        }
-            
-        }).catch((err)=>{
-            console.log(err)
-            
-        });
-    }
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      if (res.status == 200) {
+        document.getElementById("Name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("Message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+      } else {
+        console.log("gayana text");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
